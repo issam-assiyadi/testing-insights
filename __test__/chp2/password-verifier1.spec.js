5354f560a9fe05c9cfe417df77676bd3bb043965
issam-assiyadi <assiyad.issam@gmail.com>
2025-04-28 23:00:43 +0100

    feat: add password-verfier1 unit test

---
+++ __test__/chp2/password-verifier1.spec.js
@@ -0,0 +1,29 @@
+const { PasswordVerifier1 } = require("../../PasswordVerifier");
+
+describe("PasswordVerifier1", () => {
+  let verifier;
+  beforeEach(() => (verifier = new PasswordVerifier1()));
+
+  describe("with a failing rule", () => {
+    let errors;
+
+    beforeEach(() => {
+      // GIVEN
+      fakeRule = (input) => ({ passed: false, reason: "fake reason" });
+      verifier.addRule(fakeRule);
+
+      // WHEN
+      errors = verifier.verify("any value");
+    });
+
+    it("has an error message based on the rule.reason", () => {
+      // THEN
+      expect(errors[0]).toContain("fake reason");
+    });
+
+    it("has exactly one error", () => {
+      // THEN
+      expect(errors.length).toBe(1);
+    });
+  });
+});
