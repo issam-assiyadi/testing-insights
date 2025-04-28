e6fa82e2aaefb1e2592922a39ff87e4afe4c2a71
issam-assiyadi <assiyad.issam@gmail.com>
2025-04-28 22:59:37 +0100

    chore: add a class base implementation of PasswordVerifier

--- PasswordVerifier/index.js
+++ PasswordVerifier/index.js
@@ -9,6 +9,28 @@ const verifyPassword = (input, rules) => {
   return errors;
 }
 
+class PasswordVerifier1 {
+  constructor() {
+    this.rules = [];
+  }
+
+  addRule (rule) {
+    this.rules.push(rule);
+  }
+
+  verify (input) {
+    const errors = [];
+    this.rules.forEach((rule) => {
+      const result = rule(input);
+      if (result.passed == false) {
+        errors.push(result.reason);
+      }
+    })
+    return errors;
+  }
+}
+
 module.exports = {
   verifyPassword,
+  PasswordVerifier1
 }
